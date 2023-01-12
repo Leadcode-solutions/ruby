abf9024f79f308eb793ba764018955df5f67298f
NathaelB <kirua48000@gmail.com>
2023-01-13 00:10:07 +0100

    :construction: init group Authentication

---
+++ apps/Public/Controllers/AuthController.ts
@@ -0,0 +1,28 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async login ({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      await auth.use('api').attempt(email, password)
+      response.send({
+        message: "Vous vous êtes bien connecté",
+        user: auth.user
+      })
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+
+  public async logout ({}: HttpContextContract) {}
+
+  public async me ({ auth }: HttpContextContract) {
+    const user = auth.user
+
+    return {
+      user: user
+    }
+  }
+}
--- apps/Public/routes.ts
+++ apps/Public/routes.ts
@@ -2,4 +2,14 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'TestController.foo')
+
+  Route.group(() => {
+    Route.post('/login', 'AuthController.login')
+    Route.group(() => {
+      Route.get('/me', 'AuthController.me')
+      Route.post('/logout', 'AuthController.logout')
+    }).middleware('auth')
+  }).prefix('authentication')
+
 }).namespace('App/Public/Controllers')
+  .domain('api.localhost')
