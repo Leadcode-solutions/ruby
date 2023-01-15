75f048d8d12cb53529b4c69dd891f0936ab06bee
NathaelB <kirua48000@gmail.com>
2023-01-15 16:23:00 +0100

    :construction: add relation `ManyToMany` between `Image` & `Realisation`

--- domains/Realisations/Models/Realisation.ts
+++ domains/Realisations/Models/Realisation.ts
@@ -1,8 +1,9 @@
-import {BaseModel, beforeCreate, column} from '@ioc:Adonis/Lucid/Orm'
+import {BaseModel, beforeCreate, column, ManyToMany, manyToMany} from '@ioc:Adonis/Lucid/Orm'
 import {slugify} from "@ioc:Adonis/Addons/LucidSlugify";
 import {attachment, AttachmentContract} from "@ioc:Adonis/Addons/AttachmentLite";
 import {randomUUID} from "node:crypto";
 import {DateTime} from "luxon";
+import Image from "Domains/Shared/Models/Image";
 
 export default class Realisation extends BaseModel {
   @column({ isPrimary: true })
@@ -36,6 +37,9 @@ export default class Realisation extends BaseModel {
 
   @column()
   public isPin: boolean
+  
+  @manyToMany(() => Image)
+  public images: ManyToMany<typeof Image>
 
   @column.dateTime({ autoCreate: true, serialize: (value: DateTime | null) => {
       return value ? value.toFormat('yyyy-L-mm') : value
