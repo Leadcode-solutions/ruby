bce64d232cf49357cf2aa38161237cabf25b0362
NathaelB <kirua48000@gmail.com>
2023-01-29 21:01:32 +0100

    :construction: ready for test

--- apps/Manager/Controllers/RolesController.ts
+++ apps/Manager/Controllers/RolesController.ts
@@ -1,30 +1,46 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Role from "Domains/Users/Models/Role";
-import {RoleStoreValidator} from "App/Manager/Validators/RoleValidator";
+import {RoleStoreValidator, RoleUpdateValidator} from "App/Manager/Validators/RoleValidator";
 
 export default class RolesController {
   public async index ({ bouncer }: HttpContextContract) {
     await bouncer.with('RolePolicy').authorize('view')
-    return Role.query();
+    return Role.query()
   }
 
   public async show ({ bouncer, params }: HttpContextContract) {
     await bouncer.with('RolePolicy').authorize('view')
-    return Role.findOrFail(params.id)
+    return Role.query().where('id', params.id).preload('permissions')
   }
 
-  public async store ({ bouncer, request, auth }: HttpContextContract) {
+  public async store ({ bouncer, request }: HttpContextContract) {
     await bouncer.with('RolePolicy').authorize('view')
     const data = await request.validate(RoleStoreValidator)
 
     return Role.create(data)
   }
 
-  public async update ({ bouncer, params, request }: HttpContextContract) {
+  public async update ({ bouncer, params, request, response }: HttpContextContract) {
     await bouncer.with('RolePolicy').authorize('view')
+    const data = await request.validate(RoleUpdateValidator)
+    const role = await Role.findOrFail(params.id)
+
+    await role.merge(data).save()
+
+    return response.send({
+      message: `Le role ${role.id} a bien été modifié`,
+      role: role
+    })
   }
 
-  public async destroy ({ bouncer, params }: HttpContextContract) {
+  public async destroy ({ bouncer, params, response }: HttpContextContract) {
     await bouncer.with('RolePolicy').authorize('view')
+    const role = await Role.findOrFail(params.id)
+    await role.delete()
+
+    return response.send({
+      message: `Le role ${role.id} a bien été supprimé`,
+      role: role
+    })
   }
 }
