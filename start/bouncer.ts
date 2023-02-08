69aa6378c9c4d7d6cdbea15105f47c8b2d6f1c12
Azarow <contactazarox@gmail.com>
2023-02-08 01:58:24 +0100

    fix:  import ManagerPolicies -> ManagerPolicy

--- start/bouncer.ts
+++ start/bouncer.ts
@@ -6,7 +6,7 @@
  */
 
 import Bouncer from '@ioc:Adonis/Addons/Bouncer'
-import ManagerPolicies from 'App/Manager/Policies'
+import ManagerPolicy from 'App/Manager/Policies'
 /*
 |--------------------------------------------------------------------------
 | Bouncer Actions
@@ -55,5 +55,5 @@ export const { actions } = Bouncer
 |****************************************************************
 */
 export const { policies } = Bouncer.registerPolicies({
-  ...ManagerPolicies
+  ...ManagerPolicy
 })
