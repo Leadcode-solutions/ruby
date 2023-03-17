e3d2e08942ba7a4c7c97450b54ae6485737252ff
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-03-17 21:42:37 +0100

    feat: Add many constraints

--- apps/Manager/Validators/BlogArticleValidator.ts
+++ apps/Manager/Validators/BlogArticleValidator.ts
@@ -5,12 +5,25 @@ export default class BlogArticleValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    label: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-    description: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+    label: schema.string.optional({ trim: true }, [
+      rules.maxLength(255),
+      rules.requiredWhen('is_visible', '=', true)
+    ]),
+    description: schema.string.optional({ trim: true }, [
+      rules.maxLength(255),
+      rules.requiredWhen('is_visible', '=', true)
+    ]),
     structure: schema.array().anyMembers(),
-    image: schema.string.nullableAndOptional({ trim: true }),
-    published_at: schema.date.nullableAndOptional(),
-    blog_category_id: schema.number.optional([rules.exists({ table: 'blog_categories', column: 'id' })])
+    image: schema.string.nullableAndOptional({ trim: true }, [
+      rules.requiredWhen('is_visible', '=', true)
+    ]),
+    published_at: schema.date.nullableAndOptional({}, [
+      rules.requiredWhen('is_visible', '=', true)
+    ]),
+    blog_category_id: schema.number.optional([
+      rules.exists({ table: 'blog_categories', column: 'id' })
+    ]),
+    is_visible: schema.boolean.optional()
   })
 
   public messages: CustomMessages = {}
